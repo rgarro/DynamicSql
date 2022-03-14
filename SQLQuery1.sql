08be8496429d805b9f2263545829e1840ef519ff
Rolando <rgarro@gmail.com>
2022-03-14 08:48:46 -0600

    spending most of their lives living in gangstas paradise

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,10 @@ sp_executesql N'SELECT COUNT(*) FROM Person.Person';
 
 DECLARE @sql_command NVARCHAR(MAX) = 'SELECT COUNT(*) FROM Person.Person';
 EXEC sp_executesql @sql_command;
+
+DECLARE @CMD NVARCHAR(MAX);
+DECLARE @search_criteria NVARCHAR(1000);
+SELECT @CMD = 'SELECT * FROM Person.Person WHERE Person.Person.LastName=';--+CONCAT(CHAR(39));
+SELECT @search_criteria = CONCAT(CHAR(39),'Smith',CHAR(39));--'Smith"; SELECT * FROM sys.tables WHERE "" = "';
+SELECT @CMD = @CMD + @search_criteria;
+EXEC sp_executesql @CMD;
